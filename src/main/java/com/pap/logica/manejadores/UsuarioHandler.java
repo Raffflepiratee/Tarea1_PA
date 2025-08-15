68dafc8b781cd41ba3691e7ba8243eb11420217b
sosam1 <sosam1763@gmail.com>
2025-08-15 19:18:55 -0300

    handler pelados

---
+++ src/main/java/com/pap/logica/manejadores/UsuarioHandler.java
@@ -0,0 +1,13 @@
+package com.pap.logica.manejadores;
+
+public class UsuarioHandler {
+    private static UsuarioHandler instancia = null;
+
+    private UsuarioHandler(){}
+
+    public static UsuarioHandler getInstancia() {
+        if (instancia == null)
+            instancia = new UsuarioHandler();
+        return instancia;
+    }
+}
