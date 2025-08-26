bd2e6092749741d6173cf514dbb55ff86f2ab817
Juligon1 <julipeque2004@gmail.com>
2025-08-26 18:11:51 -0300

    se trato de empezar prestamo, hay dudas

--- src/main/java/logica/controladores/PrestamoController.java
+++ src/main/java/logica/controladores/PrestamoController.java
@@ -16,8 +16,16 @@ public class PrestamoController implements IPrestamoController {
     }
 
     @Override
-    public void agregarPrestamo(Prestamo prestamo) {
-        prestamos.add(prestamo);
+    public void agregarPrestamo(Prestamo prestamo, String correoLector, String correoBiblio, int idMaterial) {
+        PrestamoHandler pH = PrestamoHandler.getInstancia();
+        Prestamo p = pH.buscarPrestamoPorId(prestamo.getIdPrestamo());
+        //TENEMOS DUDAS CON EL ACCESO A LOS OTROS CONTROLADORES
+        //SERA QUE ES UN UNICO CONTROLADOR CON 3 MANEJADORES?
+        if (p == null) {
+            prestamos.add(prestamo);
+            pH.agregarPrestamoH(prestamo);
+
+        }
     }
 
     @Override
