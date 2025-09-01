a5dbfef6ffee9a59721c3d436a0500ad8a263aeb
alejo <alejofocco09@gmail.com>
2025-09-01 20:10:42 -0300

    OPCIONAL: listar prestamos activos por lector queda funcionando

--- src/main/java/interfaces/IPrestamoController.java
+++ src/main/java/interfaces/IPrestamoController.java
@@ -7,7 +7,8 @@ import logica.clases.Prestamo;
 import excepciones.PrestamoRepetidoException;
 
 public interface IPrestamoController {
-    void agregarPrestamo(Date fechaSoli, Date fechaDev, EstadosP estadoP, String correoLector, String correoBiblio, int idMaterial) throws PrestamoRepetidoException;
+    void agregarPrestamo(Date fechaSoli, Date fechaDev, EstadosP estadoP, String correoLector, String correoBiblio,
+            int idMaterial) throws PrestamoRepetidoException;
 
     void eliminarPrestamo(Prestamo prestamo);
 
@@ -36,4 +37,6 @@ public interface IPrestamoController {
     List<DtPrestamo> obtenerDtPrestamosPorZona(Zonas zona);
 
     List<DtPrestamo> obtenerDtPrestamosPendientes();
+
+    List<DtPrestamo> obtenerPrestamosActivosLector(String correoLector);
 }
