a88b81c0eb4ffa3dd72a25b37032c5a13e61bd68
Rafa Boglia <rboglia21@gmail.com>
2025-09-05 21:04:44 -0300

    Presentacion: Quedó separado ListarPrestamos.

--- src/main/java/presentacion/principal.java
+++ src/main/java/presentacion/principal.java
@@ -4,23 +4,27 @@ import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.SwingConstants;
 
 import interfaces.IMaterialController;
 import interfaces.IPrestamoController;
 import interfaces.IUsuarioController;
 import interfaces.Fabrica;
 
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+// import java.awt.event.ActionListener;
+// import java.awt.event.ActionEvent;
 
 public class Principal {
     
     private JFrame frame;
 
     private RegistrarPrestamo registrarPrestamoInternalFrame;
+    private ListarPrestamos listarPrestamosInternalFrame;
 
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
@@ -55,23 +59,44 @@ public class Principal {
             (desktopSize.height- jInternalFrameSize.height)/2);
         registrarPrestamoInternalFrame.setVisible(false);
         frame.getContentPane().add(registrarPrestamoInternalFrame);
+
+        listarPrestamosInternalFrame = new ListarPrestamos(IprestamoCont);
+        jInternalFrameSize = listarPrestamosInternalFrame.getSize();
+        listarPrestamosInternalFrame.setLocation((desktopSize.width - jInternalFrameSize.width)/2,
+            (desktopSize.height- jInternalFrameSize.height)/2);
+        listarPrestamosInternalFrame.setVisible(false);
+        frame.getContentPane().add(listarPrestamosInternalFrame);
     }
 
     private void initialize(){
         frame = new JFrame();
-        frame.setBounds(100, 100, 800, 600);
+        frame.setBounds(100, 100, 1000, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setLayout(null);
 
+        //Timido mensaje de bienvenida
+        JLabel bienvenidaLabel = new JLabel("Bienvenido al Sistema de Gestión de Biblioteca");
+        bienvenidaLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        bienvenidaLabel.setVerticalAlignment(SwingConstants.CENTER);
+        bienvenidaLabel.setBounds(0, 0, frame.getWidth(), 60);
+        frame.getContentPane().add(bienvenidaLabel);
+
+        //Barra de menu
         JMenuBar menuBar = new JMenuBar();
         menuBar.setBounds(0, 0, 800, 21);
         frame.getContentPane().add(menuBar);
 
+        //Opciones de menu
         JMenu mnPrestamo = new JMenu("Préstamo");
         menuBar.add(mnPrestamo);
 
+        //Items de menu 'Prestamo'
         JMenuItem mntmRegistrarPrestamo = new JMenuItem("Registrar Préstamo");
         mntmRegistrarPrestamo.addActionListener(e-> registrarPrestamoInternalFrame.setVisible(true));
         mnPrestamo.add(mntmRegistrarPrestamo);
+
+        JMenuItem mntmListarPrestamos = new JMenuItem("Listar Préstamos");
+        mntmListarPrestamos.addActionListener(e-> listarPrestamosInternalFrame.setVisible(true));
+        mnPrestamo.add(mntmListarPrestamos);
     }
 }
